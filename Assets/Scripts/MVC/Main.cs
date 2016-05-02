a05cd016c73f9ea96bed4f53c161a1cae824f12c
James Kong <k.kong@actimirror.com>
2016-05-03 00:06:48 +0800

    update UI

--- Assets/Scripts/MVC/Main.cs
+++ Assets/Scripts/MVC/Main.cs
@@ -3,11 +3,28 @@ using System.Collections;
 using System;
 
 public class Main : MonoBehaviour {
-	
+	public GameObject canvas;
+	private bool isShowing;
 	void Start()
 	{
+
 	}
 	void Update () {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			// Get movement of the finger since last frame
+//			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+//
+//			// Move object across XY plane
+//			transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+
+			isShowing = !isShowing;
+			canvas.SetActive(isShowing);
+
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			isShowing = !isShowing;
+			canvas.SetActive(isShowing);
+		}
 	}
 
 }
