06ab9798bc6a1c566a77cc4f77c63a01632b14e1
James Kong <k.kong@actimirror.com>
2016-05-04 19:51:18 +0800

    update about view

--- Assets/Scripts/Effects/CameraRotateEffect.cs
+++ Assets/Scripts/Effects/CameraRotateEffect.cs
@@ -6,45 +6,60 @@ using AssemblyCSharp;
 
 
 
-public class CameraRotateEffect : MonoBehaviour {
+public class CameraRotateEffect : MonoBehaviour
+{
 	public GameObject sphere;
 	public GameObject[] audioSources;
 
-//	private float _smoothTime = 0.3F;
-//	private float yVelocity = 0.0F;
+	private float _smoothTime = 0.1F;
+	private float yVelocity = 0.1F;
 	private Time time;
 	private float lastAngle;
 	private float heading = 0;
 	// Use this for initialization
-	void Start () {
+	private float target; 
+	void Start ()
+	{
 		lastAngle = 0;
 		
-		for(int i = 0 ; i < audioSources.Length ; i++){
-			audioSources[i].transform.LookAt (Camera.main.transform);
-			SpriteRenderer renderer = audioSources [i].GetComponents<SpriteRenderer>()[0];
-			renderer.color = new Color(0f, 0.0f, 1.0f, 1f); 
+		for (int i = 0; i < audioSources.Length; i++) {
+			audioSources [i].transform.LookAt (Camera.main.transform);
+			SpriteRenderer renderer = audioSources [i].GetComponents<SpriteRenderer> () [0];
+			renderer.color = new Color (0f, 0.0f, 1.0f, 1f); 
 		}
 	
 //		Camera.main.transform.LookAt (transform);
 	}
 	
 	// Update is called once per frame
-	void Update() {
+	void Update ()
+	{
 		float diff = heading - lastAngle;
-		Camera.main.transform.RotateAround(transform.position, Vector3.up, diff);
-		for(int i = 0 ; i < audioSources.Length ; i++){
-			audioSources[i].transform.LookAt (Camera.main.transform);
-			float dist = Math.Abs(Vector3.Distance(audioSources[i].transform.position, transform.position));
-			SpriteRenderer renderer = audioSources[i].GetComponents<SpriteRenderer>()[0];
-			renderer.color = HSBColor.ToColor(
-				new HSBColor(
-					Utils.Map(150.0f,0.0f,256.0f,0.0f,1.0f),
-					Utils.Map(dist,10.0f, 0.0f, 0.0F, 1.0f),
+		Camera.main.transform.RotateAround (transform.position, Vector3.up, diff);
+		for (int i = 0; i < audioSources.Length; i++) {
+			audioSources [i].transform.LookAt (Camera.main.transform);
+			float dist = Math.Abs (Vector3.Distance (audioSources [i].transform.position, transform.position));
+			SpriteRenderer renderer = audioSources [i].GetComponents<SpriteRenderer> () [0];
+			renderer.color = HSBColor.ToColor (
+				new HSBColor (
+					Utils.Map (150.0f, 0.0f, 256.0f, 0.0f, 1.0f),
+					Utils.Map (dist, 10.0f, 0.0f, 0.0F, 1.0f),
 					1.0f,
 					1f)); 
 		}
-		lastAngle = Mathf.Lerp (lastAngle, heading, Time.deltaTime); 
-		heading = Mathf.Lerp(heading, -Input.compass.trueHeading, Time.deltaTime*0.5f);
+		if (diff > 0.1) {
+			lastAngle = (heading - lastAngle) * 0.001f;
+		}
+		if (Input.compass.enabled) {
+			target = -Input.compass.trueHeading;
+			heading = Mathf.Lerp (heading, target, Time.deltaTime * 20.0f);
+		} else {
+			target = -360 + Time.deltaTime;
+			heading = Mathf.SmoothDamp (heading, target, ref yVelocity, _smoothTime);
+
+
+		}
+
 		Camera.main.transform.LookAt (sphere.transform);
 	}
 }
--- Assets/Scripts/MVC/User.cs
+++ Assets/Scripts/MVC/User.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class User : MonoBehaviour {
+	public float maxAge;
 	public float age;
 	public string uid;
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("User Created");
+		age = UnityEngine.Random.value * maxAge + maxAge * 0.5f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		age = age * 0.01f;
+		if (age < 1) {
+			Destroy (this);
+		}
+		transform.localScale *= age;		
 	}
 }
